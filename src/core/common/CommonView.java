ceac3c629a6a3e17b2b7935370db9ff25c16b600
cothis <minho1030@naver.com>
2020-12-27 21:46:32 +0900

    add CommonView

---
+++ src/core/common/CommonView.java
@@ -0,0 +1,12 @@
+package core.common;
+
+public class CommonView {
+
+    public static void printSubject(String subject) {
+        System.out.println("-----" + subject + "-----");
+    }
+
+    public static void printMessage(String message) {
+        System.out.println("\"" + message + "\"");
+    }
+}
--- src/core/common/InputValidator.java
+++ src/core/common/InputValidator.java
@@ -26,6 +26,5 @@ public class InputValidator {
             }
         }
         return result;
-
     }
 }
