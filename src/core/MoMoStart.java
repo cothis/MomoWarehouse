2dade4d0844e087bb0c95ad2966d35ff3e448df8
cothis <minho1030@naver.com>
2020-12-28 16:30:59 +0900

    add logo

--- src/core/MoMoStart.java
+++ src/core/MoMoStart.java
@@ -1,8 +1,11 @@
 package core;
 
+import core.common.CommonView;
+
 public class MoMoStart {
 	public static void main(String[] args) {
 		ApplicationConfig app = new ApplicationConfig();
+		CommonView.logo();
 		app.start();
 	}
 }
